abd4ae53c32c592160cd81ef8fa10296b3758b23
bilhudapramana <bilhuda.pramana04@gmail.com>
2026-03-09 16:03:28 +1100

    feat: Introduce initial application setup with home and shipment import pages, styling, and cartage advice parsing utility.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import AddContainer from './pages/AddContainer';
+import ImportShipment from './pages/ImportShipment';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/add" element={<AddContainer />} />
+        <Route path="/import" element={<ImportShipment />} />
       </Routes>
     </BrowserRouter>
   );
